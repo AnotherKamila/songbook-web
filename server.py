4b476ffe39329ef37aee962d1031a748b318fbdd
Kamila Součková <kamila@ksp.sk>
2016-01-06 21:12:37 +0100

    fix server.py for heroku

--- server.py
+++ server.py
@@ -6,9 +6,8 @@ class App(object):
     def index(self):
         return "Hello World!"
 
-if __name__ == '__main__':
-    cherrypy.config.update({
-        'server.socket_host': '127.0.0.1',
-        'server.socket_port': int(os.environ.get('PORT', '5000')),
-    })
-    cherrypy.quickstart(App())
+cherrypy.config.update({
+    'server.socket_host': '0.0.0.0',
+    'server.socket_port': int(os.environ.get('PORT', '5000')),
+})
+cherrypy.quickstart(App())
