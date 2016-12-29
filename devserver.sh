5cb6fcc126348bcf9e65ab0a94069820e749af3a
Kamila Souckova <kamila@ksp.sk>
2016-12-29 17:52:06 +0100

    new package.json

--- devserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# launches webpack-dev-server with the correct options (to make it behave like server.js)
-
-BASE='public/'
-$(npm bin)/webpack-dev-server --inline --hot --content-base "$BASE" --history-api-fallback
