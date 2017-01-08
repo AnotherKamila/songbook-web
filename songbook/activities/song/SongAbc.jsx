05243ed88c4011e5f9cdcbe12b21bfafa7a2494b
Kamila Souckova <kamila@ksp.sk>
2017-01-08 21:44:19 +0100

    add transposition to sheet music!!!

--- songbook/activities/song/SongAbc.jsx
+++ songbook/activities/song/SongAbc.jsx
@@ -4,15 +4,24 @@
 // and therefore webpack completely refuses to bundle it. Argh.
 
 import React from 'react'
+import {DropDownMenu, MenuItem} from 'material-ui'
 import Measure from 'react-measure'
+import _ from 'lodash'
 
 import './song.sass'
+
+const TONES = _.range(-12, 13) // half-open :D
+const Transpose = ({value, onChange}) => (
+    <DropDownMenu value={value} onChange={(e, i, val) => onChange(val)} maxHeight={200}>
+        {TONES.map(i => <MenuItem key={i} value={i} label={'Transpose: '+i} primaryText={i ? i+' semitones' : 'None'} />)}
+    </DropDownMenu>
+)
 
 export class SongAbc extends React.Component {
     constructor(props) {
         super(props)
         this.svg = '' // NOT using state here, because it was eating some calls to setState...
-        this.state = {}
+        this.state = {transpose: 0}
     }
 
     add_svg_chunk = chunk => {
@@ -23,6 +32,7 @@ export class SongAbc extends React.Component {
     rerender = () => {
         if (!this.state.width) return; // ...and call me in a while
         this.svg = ''
+        let transpose = '%%transpose '+this.state.transpose // WHOA! :D
         let size=[ // why not :D
             '%%pagewidth '+(this.state.width),
             '%%leftmargin 0',
@@ -32,21 +42,33 @@ export class SongAbc extends React.Component {
             img_out: this.add_svg_chunk,
             errmsg: console.warn, // TODO
             read_file: fname => console.warn('Includes not supported (requested '+fname+')'), // TODO
-        }).tosvg('[input]', size+'\n'+this.props.song)
+        }).tosvg('[abc]', [transpose, size, this.props.song].join('\n'))
     }
 
     componentDidUpdate(prev_props, prev_state) {
-        if (prev_state.width != this.state.width) this.rerender()
-        if (prev_props.song  != this.props.song)  this.rerender()
+        if (prev_state.width     != this.state.width ||
+            prev_state.transpose != this.state.transpose ||
+            prev_props.song      != this.props.song) this.rerender()
     }
 
     render() {
+        let div = null
+        if (!this.state.svg) {
+            div = <div className="song-abc">Rendering...</div>
+        } else {
+            div = (
+                <div className="song-abc">
+                    <div className='song-transpose'>
+                        <Transpose value={this.state.transpose}
+                                         onChange={(val) => this.setState({transpose: val})} />
+                    </div>
+                    <div dangerouslySetInnerHTML={{__html: this.state.svg}} />
+                </div>
+            )
+        }
         return (
             <Measure onMeasure={({width}) => this.setState({width: width})} >
-                {this.state.svg ?
-                    <div className="song-abc" dangerouslySetInnerHTML={{__html: this.state.svg}} /> :
-                    <div className="song-abc">Rendering...</div>
-                }
+                {div}
             </Measure>
         )
     }
