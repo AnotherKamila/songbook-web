dc8d2628300941fbb5809557713cb2a2eb9c26e8
Kamila Souckova <kamila@ksp.sk>
2017-01-10 16:00:02 +0100

    what happened to the previous commit?!

--- songbook/activities/intro/index.jsx
+++ songbook/activities/intro/index.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {FormattedMessage as T, injectIntl} from 'react-intl'
+
+import {LangSelect} from '../settings/Settings.jsx'
+import {CHANGE} from '../settings/actions'
 import en_text from 'html!markdown!./en.md'
 import sk_text from 'html!markdown!./sk.md'
 
-export const Intro = injectIntl(({intl}) => (
+export const Intro = connect(
+    state => ({
+        language: state.settings.language,
+    }),
+    dispatch => ({
+        onLanguageChange: (lang) => {
+            dispatch(CHANGE({language: lang}))
+        },
+    })
+)(injectIntl(props => (
     <div className="songbook-wrapper">
         <div className="songbook-content padded">
+            <div className='header-right'>
+                <LangSelect value={props.language}
+                            onChange={props.onLanguageChange} />
+            </div>
             <h1><T id="intro.title"/></h1>
-            <div dangerouslySetInnerHTML={{__html: intl.locale == 'sk' ? sk_text : en_text}} />
+            <div dangerouslySetInnerHTML={{__html: props.intl.locale == 'sk' ? sk_text : en_text}} />
         </div>
     </div>
-))
+)))
--- songbook/activities/settings/Settings.jsx
+++ songbook/activities/settings/Settings.jsx
@@ -1,32 +1,24 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {FormattedMessage} from 'react-intl'
-import {Paper} from 'material-ui'
+import {DropDownMenu, MenuItem, Paper} from 'material-ui'
+import {FormattedMessage as T} from 'react-intl'
 
 import messages from '../../../translations/messages.yml'
 
 import {CHANGE} from './actions.js'
 
-class LangSelect extends React.Component {
-    handleChange(e) {
-        return this.props.onChange(e.target.value)
-    }
-
-    render() {
-        return (
-            <p>
-                <label htmlFor="lang-select">
-                    <FormattedMessage id='settings.label.lang' />:
-                </label>
-                <select id='lang-select' value={this.props.value} onChange={this.handleChange.bind(this)}>
-                    {Object.keys(messages).map(lang =>
-                        <option key={lang} value={lang}>{lang}</option>
-                    )}
-                </select>
-            </p>
-        )
-    }
-}
+export const LangSelect = ({value, onChange}) => (
+    <DropDownMenu value={value} onChange={(e, i, val) => onChange(val)}>
+        {Object.keys(messages).map(lang => (
+            <MenuItem value={lang}
+                      label={<T id='settings.label.lang' values={{lang: lang}}/>}
+                      primaryText={lang}
+                      key={lang} />
+            )
+        )}
+    </DropDownMenu>
+)
 LangSelect.propTypes = {
     onChange: React.PropTypes.func,
     value: React.PropTypes.string.isRequired,
