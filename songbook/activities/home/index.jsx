635b1095e893cd4005b74fffee34bd9f1ee7d1b0
Kamila Souckova <kamila@ksp.sk>
2017-04-19 23:59:30 +0200

    WIP, button with recaptcha

--- songbook/activities/home/index.jsx
+++ songbook/activities/home/index.jsx
@@ -5,6 +5,8 @@ import {FlatButton} from 'material-ui'
 
 const google_login = (response) => console.log('google_login', response)
 
+// Note to self: window.crypto.getRandomValues()
+
 class SignIn extends React.Component {
     constructor(props) {
         super(props)
@@ -20,6 +22,7 @@ class SignIn extends React.Component {
                     auth.isSignedIn.listen(this.signin_changed)
                     auth.currentUser.listen(this.user_changed)
                     if (auth.isSignedIn.get()) auth.signIn()
+                    this.auth = auth
                 },
                 (error) => console.log('google login init error', error)
             )
@@ -28,26 +31,21 @@ class SignIn extends React.Component {
 
     user_changed = (user) => {
         console.log('user_changed', user)
-        const g_profile = user.getBasicProfile()
-        this.setState({
-            profile: {
-                name: g_profile.getName(),
-                id: g_profile.getId(),
-                image: g_profile.getImageUrl()
-            },
-        })
+        if (user.isSignedIn()) {
+            const g_profile = user.getBasicProfile()
+            this.setState({
+                profile: {
+                    name: g_profile.getName(),
+                    id: g_profile.getId(),
+                    image: g_profile.getImageUrl()
+                },
+            })
+        } else {
+            this.setState({profile: null})
+        }
     }
 
-    sign_out = () => {
-        this.auth.signOut().then(() => {console.log('User signed out.')})
-    }
-
-    componentDidMount() {
-        gapi.signin2.render('g-signin2', {
-            onsuccess: this.signin_changed,
-            onerror: (err) => console.log(err),
-        })
-    }
+    sign_out = () => this.auth.signOut()
 
     render() {
         let img = this.state.profile ? <img src={this.state.profile.image} /> : ''
@@ -56,17 +54,30 @@ class SignIn extends React.Component {
                 <div className="g-signin2"></div>
                 <span>Stuff: {JSON.stringify(this.state.profile)}</span>
                 {img}
-                <FlatButton onClick={this.sign_out}>Sign Out</FlatButton>
+                {this.state.profile ? <FlatButton onClick={this.sign_out}>Sign Out</FlatButton> : ''}
             </div>
         )
     }
 }
 
+// Note to self: You will need to call grecaptcha.reset() to ask the end user to verify with reCAPTCHA again.
+// Note to self: grecaptcha.getResponse(opt_widget_id) instead of callback, maybe
+const ReCaptcha = ({on_submit}) => (
+    <button className="g-recaptcha"
+            data-sitekey="6Lf2xx0UAAAAABnnEZqkmeCkK1MmewlCL9KjTJze"
+            data-callback={on_submit}>
+        Submit
+    </button>
+)
+
+
+window.recaptcha_submitted = (token) => console.log('recaptcha', token)
 export const Home = props => (
     <div className="content-wrapper">
         <div className='content padded'>
             <T id='home.you_should_sign_in' />
             <SignIn/>
         </div>
+        <ReCaptcha on_submit="recaptcha_submitted"/>
     </div>
 )
